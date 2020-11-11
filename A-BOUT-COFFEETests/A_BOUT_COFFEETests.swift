179fe569b2ba506758edf2825a9e9c4c26a45dfd
leeminjae <qlqlzbto@naver.com>
2020-11-11 16:36:42 +0900

    setting

---
+++ A-BOUT-COFFEETests/A_BOUT_COFFEETests.swift
@@ -0,0 +1,34 @@
+//
+//  A_BOUT_COFFEETests.swift
+//  A-BOUT-COFFEETests
+//
+//  Created by 이민재 on 10/11/2020.
+//  Copyright © 2020 이민재. All rights reserved.
+//
+
+import XCTest
+@testable import A_BOUT_COFFEE
+
+class A_BOUT_COFFEETests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
